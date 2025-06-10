a96822360d846a9ec74ff4fb8a9a975045da1db1
Beatriz Pedrosa <biapedrosa17@gmail.com>
2025-06-10 00:58:37 -0300

    Ajeitando erros no JavaScript

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
 function alternarTema() {
-    document.body.classList.toggle("escuro");
-  if (body.style.backgroundColor === 'white') {
-    body.style.backgroundColor = 'black';
-    body.style.color = 'white';
-  } else {
-    body.style.backgroundColor = 'white';
-    body.style.color = 'black';
-  }
+  document.body.classList.toggle("escuro");
 }
